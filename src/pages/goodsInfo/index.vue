e2c870ec4c3a8fa962a8594743f80c0796af2e6d
Christmas-An <839132410@qq.com>
2019-01-15 21:00:55 +0800

    商品详情（除加入购物车）

--- src/pages/goodsInfo/index.vue
+++ src/pages/goodsInfo/index.vue
@@ -4,10 +4,10 @@
     <div class="mui-card">
 				<div class="mui-card-content">
           <div class="mui-card-content-inner">
-            <mt-swipe :auto="4000">
-              <mt-swipe-item>1</mt-swipe-item>
-              <mt-swipe-item>2</mt-swipe-item>
-              <mt-swipe-item>3</mt-swipe-item>
+            <mt-swipe :auto="4000" >
+              <mt-swipe-item v-for="(item, i) in lunboList" :key="i">
+                <img :src="item.src" alt="">
+              </mt-swipe-item>
             </mt-swipe>
           </div>
         </div>
@@ -18,13 +18,16 @@
 			<div class="mui-card-content">
 				<div class="mui-card-content-inner">
           <p class="price">
-            市场价: <del>￥2499</del> &nbsp;&nbsp;销售价: <span>￥2195</span>
+            市场价: 
+            <del>￥{{ goodsList.market_price }}</del> &nbsp;&nbsp;销售价: 
+            <span>￥{{ goodsList.sell_price }}</span>
           </p>
           <p class="numBox">
             购买数量: 
             <input type="text" v-model="buyCount">
             <input type="button" :disabled="buyCount <= 1" @click="buyCount > 1 && buyCount--" value="-" class="reduce">
-            <input type="button" @click=" buyCount++" value="+" class="increase">
+            <input type="button" :disabled="buyCount >= goodsList.stock_quantity" 
+            @click="buyCount <= goodsList.stock_quantity && buyCount++" value="+" class="increase">
           </p>
           <p>
             <button class="mint-button mint-button--primary mint-button--small">立即购买</button>
@@ -38,14 +41,14 @@
       <div class="mui-card-header">商品参数</div>
 			<div class="mui-card-content">
 				<div class="mui-card-content-inner">
-          <p>商品货号：SD9102356032</p>
-          <p>库存情况：60件</p>
-          <p>上架时间：2015-04-20 00:51:03</p>
+          <p>商品货号：{{ goodsList.goods_no }}</p>
+          <p>库存情况：{{ goodsList.stock_quantity }}件</p>
+          <p>上架时间：{{ goodsList.add_time | dataFormat }}</p>
 				</div>
 			</div>
       <div class="mui-card-footer">
-        <mt-button type="primary" size="large" plain >图文介绍</mt-button>
-        <mt-button type="danger" size="large" plain >商品评论</mt-button>
+        <mt-button type="primary" size="large" plain @click="goDesc(id)">图文介绍</mt-button>
+        <mt-button type="danger" size="large" plain @click="goComment(id)">商品评论</mt-button>
       </div>
 		</div>
   </div>
@@ -55,10 +58,44 @@
 export default {
   data(){
     return {
+      id:this.$route.params.id,
       buyCount:1,
+      lunboList:[],
+      goodsList:[],
     }
   },
-  
+  created(){
+    this.getLunbo()
+    this.getGoodsInfo()
+  },
+  methods:{
+    // 获取轮播图
+    getLunbo(){
+      this.$http.get("api/getthumimages/" + this.id).then( result => {
+        // console.log(result.body)
+        if(result.body.status == 0){
+          this.lunboList = result.body.message
+        }
+      })
+    },
+    // 获取商品详情
+    getGoodsInfo(){
+      this.$http.get("api/goods/getinfo/" + this.id).then( result => {
+        // console.log(result.body)
+        if(result.body.status == 0){
+          this.goodsList = result.body.message[0]
+        }
+      })
+    },
+    // 评论页
+    goComment(id){
+      this.$router.push({ name:'goodsComment', params: { id } })
+    },
+    // 商品详情
+    goDesc(id){
+      this.$router.push({ name:'goodsDesc', params:{ id } })
+    }
+  }
 }
 </script>
 
@@ -69,6 +106,10 @@ export default {
       height: 200px;
       .mint-swipe-item {
         height: 100%;
+        img {
+          width: 340px;
+          height: 200px;
+        }
       }
     }
     // 价格
